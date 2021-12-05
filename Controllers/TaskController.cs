103df0913dd00029de7e0f219e152ea5034f8110
HiMatterhorn <wyp4tomasz@gmail.com>
2021-12-05 16:39:59 +0100

    Static values of tasks initialized in TaskController class

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagerMVC.Models;
+
+namespace TaskManagerMVC.Controllers
+{
+    public class TaskController : Controller
+    {
+        private static IList<TaskModel> tasks = new List<TaskModel>()
+        {
+            new TaskModel(){TaskId =1, Name = "Wizyta u lekarza", Description = "Godzina 17:00", Done = false },
+            new TaskModel(){TaskId =2, Name = "Zrobić obiad", Description = "Pierogi", Done = false },
+
+        };
+    
+        // GET: TaskController
+        public ActionResult Index()
+        {
+            return View(tasks);
+        }
+
+        // GET: TaskController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TaskController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TaskController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TaskController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TaskController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TaskController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TaskController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
