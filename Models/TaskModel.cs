e9bd45bee1ef2473ba274a9fab7d3e7ecf98a7e4
HiMatterhorn <wyp4tomasz@gmail.com>
2021-12-05 16:54:44 +0100

    Polish annotations added

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace TaskManagerMVC.Models
 {
     public class TaskModel
     {
         public int TaskId { get; set; }
+        [DisplayName("Nazwa")]
         public string Name { get; set; }
+        [DisplayName("Opis")]
         public string Description { get; set; }
         public bool Done { get; set; }
     }
